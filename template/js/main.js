4c44c3c9205b6d8808577b1b37c1e0e588a464cf
dipo Majekodunmi <dipo@dipolediamond.com>
2015-02-28 20:13:25 +0100

    formatted artisan-review-summary divs

--- template/js/main.js
+++ template/js/main.js
@@ -1,6 +1,7 @@
 (function() {
 	/*landing page interaction*/
-	var trade, location;
+	var recordCount, trade, location;
+	recordCount = 3;
 	//trade selected
 	$("#select-trade").change(function(){
 		trade = $("#trade").val();
@@ -11,9 +12,20 @@
 	$("#select-location").change(function(){
 		location = $("#location").val();
 		$("#select-location").hide();
-		$("#result-count").text("3");
+		$("#result-count").text(recordCount);
 		$("#search-trade").text(trade);
 		$("#search-location").text(location);
 		$(".search-results").removeClass("hidden");
+		if(recordCount > 0){
+			$(".artisans").removeClass("hidden");
+		}
+		else{
+			$("#result-count").text("no");
+			$(".no-result-action").removeClass("hidden");
+			//if guest
+			//$(".guest-message").removeClass("hidden");
+			//if logged in user
+			$(".user-message").removeClass("hidden");
+		}
 	});
 })();
